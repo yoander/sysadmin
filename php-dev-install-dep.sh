67f2ff2992e150733cdad8b42c6ad77cf08b45a2
Yoander Valdés Rodríguez <yoander.valdes@gmail.com>
2014-07-15 15:50:12 -0400

    [PHP-Build] Set correct depencies and symbolic links

--- php-dev-install-dep.sh
+++ php-dev-install-dep.sh
@@ -13,24 +13,23 @@ WEB_SRV='apache-prefork'
 #Debian/Ubuntu distro
 if egrep -i 'debian|ubuntu' /etc/issue > /dev/null; then
     case $WEB_SRV in
-        'apache-prefork') WEB_SRV_PKG='apache2 apache-mpm-prefork apache2-prefork-dev';;
+        'apache-prefork') WEB_SRV_PKG='apache2 apache2-mpm-prefork apache2-prefork-dev';;
         'apache-worker') WEB_SRV_PKG=;;
         'nginx') WEB_SRV_PKG=;;
         *) echo Unknown web server: adjust the script by yourself, bye!; exit 2;;
     esac
-    
-    $USER_ALIAS apt-get -y install make autoconf gcc libxml2-dev libssl-dev openssl \
-         libpcre3-dev libsqlite3-dev libbz2-dev libcurl4-openssl-dev libgd2-xpm-dev \
-         libicu-dev libmcrypt-dev libpq-dev libreadline-dev $WEB_SRV_PKG bison \
-         flex re2c libtool g++ libstdc++6-4.7-dev
 
-    
-    $USER_ALIAS ln -s /lib/$(arch)-linux-gnu/libpcre.so.3 /usr/lib/libpcre.so
-    $USER_ALIAS ln -s /usr/lib/gcc/$(arch)-linux-gnu/4.7/cc1 /usr/lib/gcc/$(arch)-linux-gnu/4.7/cc1plus
-    $USER_ALIAS cp -p /var/www/index.html /var/www/info.php
-    $USER_ALIAS bash -c "echo '<?php phpinfo();' > /var/www/info.php"
+   $USER_ALIAS  apt-get install -y make autoconf gcc libxml2-dev libssl-dev openssl \
+       libpcre3-dev libsqlite3-dev libbz2-dev libcurl4-openssl-dev libgd2-xpm-dev \
+       libicu-dev libmcrypt-dev libpq-dev libreadline-dev $WEB_SRV_PKG bison flex \
+       re2c libtool libstdc++6-4.7-dev
 
-    $USER_ALIAS wget https://raw.githubusercontent.com/yoander/sysadmin/master/php5.conf -O /etc/apache2/mods-available/php5.conf
+    $USER_ALIAS ln -fs /lib/$(arch)-linux-gnu/libpcre.so.3 /usr/lib/libpcre.so && \
+    $USER_ALIAS ln -fs /usr/lib/gcc/$(arch)-linux-gnu/4.7/cc1 /usr/lib/gcc/$(arch)-linux-gnu/4.7/cc1plus && \
+    $USER_ALIAS cp -p /var/www/index.html /var/www/info.php && \
+    $USER_ALIAS bash -c "echo '<?php phpinfo();' > /var/www/info.php" && \
+
+    $USER_ALIAS wget https://raw.githubusercontent.com/yoander/sysadmin/master/php5.conf -O /etc/apache2/mods-available/php5.conf && \
     $USER_ALIAS ln -s /etc/apache2/mods-available/php5.conf /etc/apache2/mods-enabled/php5.conf
 
 # Centos/RHEL/Fedora distro
